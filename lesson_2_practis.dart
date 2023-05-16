3193c0355124e8a0f9a1ef806a31aeff82983c43
chikkaa <turgunbaevcyngyz1@gmail.com>
2023-05-16 20:14:39 +0300

    first commit

---
+++ lesson_2_practis.dart
@@ -0,0 +1,186 @@
+void main () {
+num a = 123;
+print(a);
+
+num k = 464;
+print(k);
+
+num f = 123;
+print(f);
+
+num h = 123;
+print(h);
+
+num j = 123;
+print(j);
+
+int b = 3818393;
+print(b);
+
+int d = 3818393;
+print(d);
+
+int l = 3818393;
+print(l);
+
+int o = 3818393;
+print(o);
+
+int bdq = 3818393;
+print(bdq);
+
+
+double c = 12374.1313;
+print(c);
+
+double zxc = 12374.1313;
+print(zxc);
+
+double vbn = 12374.1313;
+print(vbn);
+
+double dfg = 12374.1313;
+print(dfg);
+
+double hjk = 12374.1313;
+print(hjk);
+
+
+
+String gko = "chikka";
+print(gko);
+
+String iop = "chikka";
+print(iop);
+
+String wer = "chikka";
+print(wer);
+
+String ha = "chikka";
+print(ha);
+
+String ls = "chikka";
+print(ls);
+
+List hai = [127139.13213, "chikka", false];
+print(hai);
+
+List adg = [127139.13213, "chikka", false];
+print(adg);
+
+List qwe = [127139.13213, "chikka", false];
+print(qwe);
+
+List uwu = [127139.13213, "chikka", false];
+print(uwu);
+
+List lol = [127139.13213, "chikka", false];
+print(lol);
+
+Map student  ={'chikka': 13819, 'lalala': 13717,};
+
+Map shcool  ={'chikka': 13819, 'lalala': 13717,};
+
+Map classi  ={'chikka': 13819, 'lalala': 13717,};
+
+Map home  ={'chikka': 13819, 'lalala': 13717,};
+
+Map work  ={'chikka': 13819, 'lalala': 13717,};
+
+final user ;
+user =13983;
+print(user);
+
+final user1 ;
+user1 =13983;
+print(user1);
+
+final user2 ;
+user2 =13983;
+print(user2);
+
+final user3 ;
+user3 =13983;
+print(user3);
+
+final user4 ;
+user4 =13983;
+print(user4);
+
+
+const boys =1324;
+print(boys);
+
+const boys1 =1324;
+print(boys1);
+
+const boys2 =1324;
+print(boys2);
+
+const boys3 =1324;
+print(boys3);
+
+const boys4 =1324;
+print(boys4);
+
+dynamic web = [1241, "hahahaha", false,];
+print(web);
+
+dynamic we = [1241, "hahahaha", false,];
+print(we);
+
+dynamic geb = [1241, "hahahaha", false,];
+print(geb);
+
+dynamic wab = [1241, "hahahaha", false,];
+print(wab);
+
+dynamic wel = [1241, "hahahaha", false,];
+print(wel);
+
+var heart_symbol = '\u2665';
+
+var laugh_symbol = '\u{1f600}';
+
+print(heart_symbol);
+
+print(laugh_symbol);
+
+
+var home_work = '\u{1828}';
+
+var class_work = '\u{1834}';
+
+print(home_work);
+
+print(class_work);
+
+var home_wor = '\u{1828}';
+
+var class_wor = '\u{1834}';
+
+print(home_wor);
+
+print(class_wor);
+
+var home_wo = '\u{1828}';
+
+var class_wo = '\u{1834}';
+
+print(home_wo);
+
+print(class_wo);
+
+var home_worka = '\u{1828}';
+
+var class_worka = '\u{1834}';
+
+print(home_worka);
+
+print(class_worka);
+
+
+
+
+
+}
